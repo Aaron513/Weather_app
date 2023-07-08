fcb109260f0a30950004098961cc7de4c340741e
Aaron Pereira <russellpereira05@gmail.com>
2023-07-08 12:38:25 +0530

    Created basic components for the weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+/* eslint-disable no-unused-vars */
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import Topbuttons from './components/Topbuttons.jsx';
+import Inputs from './components/Inputs';
+import TimeNLocation from './components/TimeNLocation';
+import TempNDeats from './components/TempNDeats';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
+      <Topbuttons/>
+      <Inputs/>
+
+      <TimeNLocation/>
+      <TempNDeats/>
     </div>
   );
 }
---
+++ src/components/TimeNLocation.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function TimeNLocation() {
+  return (
+    <div>
+        <div className='flex items-center justify-center my-6'>
+            <p className='text-white text-xl font-extralight'>
+                some text
+            </p>
+        </div>
+        <div className='flex items-center justify-center my-3'>
+            <p className='text-white text-3xl font-medium'>
+                other text
+            </p>
+
+        </div>
+    </div>
+  )
+}
+
+export default TimeNLocation
