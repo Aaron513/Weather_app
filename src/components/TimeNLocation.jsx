f65d36bb24b5a13afaed089598bbd2e35dae6ca4
Aaron Pereira <russellpereira05@gmail.com>
2023-08-06 23:50:00 +0530

    Completed the project.still hav to do minor changes

--- src/components/TimeNLocation.jsx
+++ src/components/TimeNLocation.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import { formatToLocalTime } from '../javascripts/weatherService'
 
-function TimeNLocation() {
+function TimeNLocation({weather: {dt, timezone, name, country}}) {
   return (
     <div>
         <div className='flex items-center justify-center my-6'>
             <p className='text-white text-xl font-extralight'>
-                some text
+                {formatToLocalTime(dt, timezone)}
             </p>
         </div>
         <div className='flex items-center justify-center my-3'>
             <p className='text-white text-3xl font-medium'>
-                other text
+                {`${name}, ${country}`}
             </p>
 
         </div>
--- src/components/Topbuttons.jsx
+++ src/components/Topbuttons.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Topbuttons() {
+function Topbuttons({setQuery}) {
 
   const cities = [
     {
@@ -27,7 +27,7 @@ function Topbuttons() {
   return (
     <div className='flex items-center justify-around my-6'>
       {cities.map((city) => (
-        <button key={city.id} className='text-white text-lg font-medium'>
+        <button key={city.id} className='text-white text-lg font-medium' onClick={() => setQuery({q: city.title})}>
           {city.title}
         </button>
       ))}
